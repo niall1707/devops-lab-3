76ee11b6a8890ee752abdd46a9b977f17ae060d8
Niall <niall1707@gmail.com>
2024-10-03 17:44:31 +0100

    Add /about route to the Flask app

--- hello.py
+++ hello.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<p>Hello, World!</p>'
+
+@app.route('/about')
+def about():
+    return '<p>This is a Flask web app running in a Linux VM.</p>'
+
